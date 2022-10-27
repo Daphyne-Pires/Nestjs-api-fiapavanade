d321ed87d89f5f6621683394718cb23c2d7ade53
Daphyne Pires <daphyne.pires@gmail.com>
2022-10-27 16:40:52 -0300

    deploy

---
+++ src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
+//https://avanade-manha.eastus2.cloudapp.azure.comhttp://20.122.118.196
